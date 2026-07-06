1363b712a40d9b722353aca2f7a243ee668ae13c
Mevludin Causevic <118404053+mevcaus@users.noreply.github.com>
2026-07-06 16:26:51 -0600

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        fleets = []
+        res = 0
+        current_blocker = 0
+        sorted_cars = sorted(zip(position, speed), key=lambda p: p[0], reverse=True)
+        for pos, sp in sorted_cars:
+            finish_time = (target - pos) / sp
+            if finish_time > current_blocker:
+                current_blocker = finish_time
+                res += 1
+        return res
