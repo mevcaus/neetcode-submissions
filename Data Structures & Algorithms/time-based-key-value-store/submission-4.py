e0fe9503829df3dce8edaf1fa378f2fcd7ef475a
Mevludin Causevic <118404053+mevcaus@users.noreply.github.com>
2026-07-09 16:04:44 -0600

    Add: time-based-key-value-store - submission-4

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-4.py
@@ -0,0 +1,27 @@
+class TimeMap:
+
+    def __init__(self):
+        self.storage = defaultdict(list)
+
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.storage[key].append([value, timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+        if len(self.storage[key]) == 0:
+            return ""
+        l,r = 0, len(self.storage[key]) - 1
+        retval = ""
+        array = self.storage[key]
+        while l <= r:
+            halfway = (l + r) // 2
+            if timestamp == array[halfway][1]:
+                return array[halfway][0]
+            elif timestamp > array[halfway][1]:
+                l = halfway + 1
+                retval = array[halfway][0]
+            else:
+                r = halfway - 1
+        return retval
+
+            
